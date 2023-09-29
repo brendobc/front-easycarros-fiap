78c1df42f675180c4932f436743b2f0770358dfe
Brendo <brendobc19@gmail.com>
2023-09-28 21:32:42 -0300

    feat: insere as validações do formulário de novo pedido

--- src/pages/novo-pedido/validar-form-novo-pedido.js
+++ src/pages/novo-pedido/validar-form-novo-pedido.js
@@ -16,12 +16,39 @@ function criarPedidoComBaseNoForm(formNovoPedido) {
     return pedido;
 }
 
+function validarCampoHiddenComModal(input, btnAbrirModal) {
+    if(
+        !input ||
+        input.value &&
+        !btnAbrirModal.classList.remove('error')
+    ) {
+        return true;
+    }
+
+    btnAbrirModal.classList.add('error');
+    return false;
+}
+
 function inserirValidadorFormNovoPedido() {
-    // TODO completar validações
+    const btnSelecionarAgencia = document.querySelector('[data-selecionar="agencia"]'),
+          inputAgenciaSelecionada = document.getElementById('agenciaId'),
+          btnSelecionarAgendamento = document.querySelector('[data-selecionar="agendamento"]'),
+          inputAgendamentoSelecionado = document.getElementById('agendamento');
+
     $('#form-novo-pedido').validate({
         ignore: ['hidden'],
         submitHandler: function(form, e) {
             e.preventDefault();
+
+            if(
+                [
+                    validarCampoHiddenComModal(inputAgenciaSelecionada, btnSelecionarAgencia),
+                    validarCampoHiddenComModal(inputAgendamentoSelecionado, btnSelecionarAgendamento)
+                ]
+                .includes(false)
+            ) {
+                return;
+            }
 
             salvarPedido(criarPedidoComBaseNoForm(form));
         }
