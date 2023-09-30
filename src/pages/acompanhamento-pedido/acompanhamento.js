34a2073e67d8c48975ea7df34e73ee2ce3dce84d
Igor Miramisawa <igor.miramisawa@outlook.com>
2023-09-30 17:40:54 -0300

    adicionando css e container no acompanhamento

--- src/pages/acompanhamento-pedido/acompanhamento.js
+++ src/pages/acompanhamento-pedido/acompanhamento.js
@@ -3,13 +3,33 @@ import { getPedidoSalvo } from "../../service/storage.js";
 console.log('Teste tela acompanhar pedido');
 
 function initConsultaAcompanhamento() {
-    const acompanhamentoPlaceholder = document.getElementById('acompanhamento');
+    const categoriaPlaceholder = document.getElementById('categoria');
+    const valorPlaceholder = document.getElementById('valor');
+    const modoPagamentoPlaceholder = document.getElementById('modoPagamento');
+    const agendamentoPlaceholder = document.getElementById('agendamento');
+    const previsaoDiasLocacaoPlaceholder = document.getElementById('previsaoDiasLocacao');
+    const nomePlaceholder = document.getElementById('nome');
+    const statusPlaceholder = document.getElementById('status');
+    const emptyPlaceHolder = document.getElementById('empty');
     
     var consultaPedido = getPedidoSalvo();
+
     if (!consultaPedido) {
-        
-        consultaPedido.innerText = "empty space" 
-    }   
+        return emptyPlaceHolder.innerText = "empty space";
+    }
+
+    const data = new Date(consultaPedido.agendamento);
+
+    var dataFormatada = data.toLocaleDateString();
+
+    categoriaPlaceholder.innerText = consultaPedido.categoria;
+    valorPlaceholder.innerText = consultaPedido.valor;
+    nomePlaceholder.innerText = consultaPedido.usuario.nome;
+    modoPagamentoPlaceholder.innerText = consultaPedido.modoPagamento;
+    agendamentoPlaceholder.innerText = dataFormatada;
+    previsaoDiasLocacaoPlaceholder.innerText = consultaPedido.previsaoDiasLocacao;
+    
+    statusPlaceholder.innerText = consultaPedido.status;
 }
 
 export {  initConsultaAcompanhamento  }
