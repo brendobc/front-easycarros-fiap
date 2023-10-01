297f646acd6e2ca9baff3322d0feaa88eaa1a743
Juliana Nonaka <juliananonakaudvari@gmail.com>
2023-10-01 01:07:05 -0300

    realizando algumas alteracoes

--- src/pages/acompanhamento-pedido/index.js
+++ src/pages/acompanhamento-pedido/index.js
@@ -1,7 +1,4 @@
 import { initConfirmarEntrega, initConsultaAcompanhamento } from "./acompanhamento.js";
 
-console.log('Teste tela acompanhar pedido');
-
 initConsultaAcompanhamento();
-
 initConfirmarEntrega();
