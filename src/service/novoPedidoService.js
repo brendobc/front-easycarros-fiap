621ec1447c45eeb68c4098d8a58a5caeaee4d683
Brendo <brendobc19@gmail.com>
2023-10-01 01:47:21 -0300

    fix: corrige os agendamentos da madrugada

--- src/service/novoPedidoService.js
+++ src/service/novoPedidoService.js
@@ -52,12 +52,10 @@ function getAgendamentosDisponiveis() {
         const horaAgendamento = agendamentos[i - 1].getHours() + 1;
 
         // Se for depois das 18h, manda para o dia seguinte
-        if(horaAgendamento <= 18) {
+        if(horaAgendamento >= 8 && horaAgendamento <= 18) {
             agendamentos[i].setHours(horaAgendamento);
-        } else {
-            agendamentos[i].setHours(8);
-            agendamentos[i].setMinutes(10);
-
+            continue;
+        } else if(horaAgendamento > 18) {
             const novaData = agendamentos[i].getDate() + 1;
 
             for(const agendamentoNovaData of agendamentos) {
@@ -65,6 +63,9 @@ function getAgendamentosDisponiveis() {
                 agendamentoNovaData.setMinutes(10);
             }
         }
+
+        agendamentos[i].setHours(8);
+        agendamentos[i].setMinutes(10);
     }
     // atenzione, atenzione! Aqui, a variável "hoje" já não corresponde ao presente momento
 
